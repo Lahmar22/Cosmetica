bc6291b8b2d859c9453202b9a1543acc77d73e84
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-17 10:29:57 +0000

    feat : add the produit model

---
+++ app/Models/Produit.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Categorie;
+
+class Produit extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'prix',
+        'images',
+        'categorie_id',
+    ];
+
+    public function categorie()
+    {
+        return $this->belongsTo(Categorie::class);
+    }
+}
