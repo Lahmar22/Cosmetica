d2935702fdf0458da86c8a9d96a8eb79b136f8bc
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-17 10:33:48 +0000

    feat : add route api for the APIs

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProduitController;
+use App\Http\Controllers\CategorieController;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/produits', [ProduitController::class, 'index']);
+    Route::post('/storeProduit', [ProduitController::class, 'store']);
+    Route::post('/destroy', [ProduitController::class, 'destroy']);
+    Route::get('/categories', [CategorieController::class, 'index']);
+    Route::post('/store', [CategorieController::class, 'store']);
+});
